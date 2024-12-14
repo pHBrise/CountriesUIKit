c69fb10a16ed65d41d0c5e697873b4f4abd44af4
Breeze <phbrise@gmail.com>
2024-12-14 16:22:37 +0700

    Initial Commit

---
+++ CountriesUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesUIKit
+//
+//  Created by Phanvit Chevamongkolnimit on 14/12/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CountriesUIKitTests/CountriesUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesUIKitTests.swift
+//  CountriesUIKitTests
+//
+//  Created by Phanvit Chevamongkolnimit on 14/12/2567 BE.
+//
+
+import Testing
+@testable import CountriesUIKit
+
+struct CountriesUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
